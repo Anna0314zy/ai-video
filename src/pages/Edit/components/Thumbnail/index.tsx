1053056d5193a165d21a3c72363e39d2abe84036
wangpeng10 <p_wangpeng10@ledupeiyou.com>
2024-07-29 19:26:38 +0800

    fix: 样式变量调整

--- src/pages/Edit/components/Thumbnail/index.tsx
+++ src/pages/Edit/components/Thumbnail/index.tsx
@@ -1,16 +1,16 @@
 /*
  * @Date: 2024-07-26 14:26:10
  * @LastEditors: wangpeng
- * @LastEditTime: 2024-07-26 15:27:54
+ * @LastEditTime: 2024-07-29 19:25:03
  * @FilePath: /ai-content-platform/src/pages/Edit/components/Thumbnail/index.tsx
  */
 import { Button, Col } from 'antd';
 import React from 'react';
-import './index.less'
+import styled from './index.module.less'
 
 const Thumbnail: React.FC = () => {
     return <Col flex="100px">
-        <ul>
+        <ul className={styled['thumbnail-list']}>
             <li>你好</li>
             <li>你好</li>
             <li>你好</li>
