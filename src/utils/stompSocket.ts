4c2fbbd821496c0139eca27f879e9db0baca2149
周东晨 <p_zhoudongchen@ledupeiyou.com>
2024-07-29 11:18:17 +0800

    feat: 添加socket

--- src/utils/stompSocket.ts
+++ src/utils/stompSocket.ts
@@ -1,45 +1,52 @@
 // import SockJS from 'sockjs-client'
-import Stomp, { Client } from 'stompjs'
+import Stomp, { Client, Frame } from 'stompjs'
+import { getToken } from '@/utils/auth'
 
 import { EventEmitter } from 'events'
 interface SocketParam {
-    baseUrl: string, // socket 地址
-    sendThorough: string, // 发送通道
-    subscribeThorough: string // 订阅地址
+  baseUrl: string // socket 地址
+  sendThorough: string // 发送通道
+  subscribeThorough: string // 订阅地址
 }
 class StompSocket extends EventEmitter {
-    
-    constructor(socketParam:SocketParam) {
-        super()
-        this.initParam = socketParam
-        this.connect()
-    }
+  constructor(socketParam: SocketParam) {
+    super()
+    this.initParam = socketParam
+    this.connect()
+  }
 
+  private initParam: SocketParam
+  private stompClient!: Client
 
-    private initParam: SocketParam
-    private stompClient!: Client
+  /**
+   * @description: 链接socket
+   * @return {*}
+   */
+  private connect() {
+    const ws = new window.SockJS(this.initParam.baseUrl)
 
-    /**
-     * @description: 链接socket
-     * @return {*}
-     */
-    private connect() {
-        const ws = new window.SockJS(this.initParam.baseUrl);
-        this.stompClient = Stomp.over(ws);
-        this.stompClient.connect({}, () => {
-            console.log('connect--')
-            this.stompClient.subscribe(this.initParam.subscribeThorough,  (message: any) => {
-                console.log('onSubscribe', message)
-                this.emit('onSubscribe', message)
-            }
-        );
-        });
-    }
+    this.stompClient = Stomp.over(ws)
+    this.stompClient.connect(
+      {
+        Authorization: getToken(),
+      },
+      () => {
+        console.log('connect--')
+        this.stompClient.subscribe(this.initParam.subscribeThorough, (message: any) => {
+          console.log('onSubscribe', message)
+          this.emit('onSubscribe', message)
+        })
+      },
+      (err) => {
+        console.log('connect-error', err)
+      }
+    )
+  }
 
-    public send() {
-        console.log('发送的消息')
-        this.stompClient.send(this.initParam.sendThorough, {}, 'hhh');
-    }
+  public send() {
+    console.log('发送的消息')
+    this.stompClient.send(this.initParam.sendThorough, {}, 'hhh')
+  }
 }
 
 export default StompSocket
