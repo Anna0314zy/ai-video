9dae06c7baa5ab634eab0a4f7871628df8289aa4
zouyu <p_zouyu@ledupeiyou.com>
2024-08-13 14:49:48 +0800

    feat: 镜头配置chat

--- src/api/models/common.ts
+++ src/api/models/common.ts
@@ -1,6 +1,6 @@
 import api from '../index'
 const http = import.meta.env.VITE_API_SERVER
-import { PathConfigList } from '../types/script'
+import { PathConfigList } from '../types/video'
 // 获取cos临时访问token
 export const getCosCredential = async (): Promise<any> => {
   return api.get(`${http}/api/cos/v1/credential?bucketName=${import.meta.env.VITE_BUCKET}`)
