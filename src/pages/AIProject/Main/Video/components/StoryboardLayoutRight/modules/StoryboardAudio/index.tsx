95606019b49e429267f13586c579c057cf217ddc
xiexiaopeng <xiexiaopeng@xiexiaopengdeMacBook-Pro.local>
2024-08-23 09:38:04 +0800

    feat: 音频增加已选效果

--- src/pages/AIProject/Main/Video/components/StoryboardLayoutRight/modules/StoryboardAudio/index.tsx
+++ src/pages/AIProject/Main/Video/components/StoryboardLayoutRight/modules/StoryboardAudio/index.tsx
@@ -203,7 +203,7 @@ export default (props: any) => {
                 onClick={() => {
                   dispatch.aiVideo.updateData({ selectedAudio: item })
                 }}
-                actived={item.resourceId === selectedAudio['resourceId']}
+                actived={item.resourceId === selectedAudio['resourceId'] || item.isFinal === 'final'}
               />
             ))
           )}
